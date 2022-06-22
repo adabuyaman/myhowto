442c8f00c49dbad31ec2cd128e58bcdd9f19f082
Ahmad Abuyaman <adabuyaman@gmail.com>
2022-06-22 20:49:04 +0300

    first commit

---
+++ src/components/header/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Navbar = ({ currentUser }) => {
+    
+    return(
+        <nav className='main-nav'>
+            <a href='#'>My-How-to</a>
+        </nav>
+    );
+}
